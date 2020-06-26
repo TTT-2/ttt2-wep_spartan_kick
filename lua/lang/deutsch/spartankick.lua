c1040abba3d76232bae5ab64db39907ed80aa5c3
Tim Goll <tim@timgoll.de>
2020-06-26 20:46:20 +0200

    update language to use new HUD help format

--- lua/lang/deutsch/spartankick.lua
+++ lua/lang/deutsch/spartankick.lua
@@ -2,6 +2,6 @@ L = LANG.GetLanguageTableReference("deutsch")
 
 L["name_spartan_kick"] = "Spartanischer Tritt"
 L["desc_spartan_kick"] = "Tritt sie aus ihrem Leben!"
-L["skick_help_msb1"] = "{primaryfire} um wie ein Spartaner zu treten"
-L["skick_help_msb2"] = "{secondaryfire} um 'Madness' Sound abzuspielen"
+L["skick_help_msb1"] = "Tritt wie ein Spartaner"
+L["skick_help_msb2"] = "Spiele 'Madness' Sound ab"
 L["skick_door_locked_indestructible"] = "Diese abgeschlossene Tür ist nicht zerstörbar."
