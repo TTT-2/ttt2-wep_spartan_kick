05365dc8baa42a5033466a6de5f245661c6a8ea3
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:09:10 +0500

    Create spartankick.lua

---
+++ lua/lang/russian/spartankick.lua
@@ -0,0 +1,7 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["name_spartan_kick"] = "Спартанский пинок"
+L["desc_spartan_kick"] = "Пните их из жизни!"
+L["skick_help_msb1"] = "Пинайте как спартанец"
+L["skick_help_msb2"] = "Проиграть звук безумия"
+L["skick_door_locked_indestructible"] = "Эта запертая дверь не разрушается."
